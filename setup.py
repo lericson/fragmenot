01766e82204b86d8d4bd77e40308d4ea1d99c551
Ludvig Ericson <ludvig@lericson.se>
2020-09-17 18:28:41 +0200

    Remove NumPy includes as they are unnecessary

--- setup.py
+++ setup.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
-
-import numpy as np
 from Cython.Build import cythonize
 
-include_path = [np.get_include()]
-
-ext_modules = cythonize('pyembree/*.pyx', language='c++',
-                        include_path=include_path)
+ext_modules = cythonize('pyembree/*.pyx', language='c++')
 for ext in ext_modules:
-    ext.include_dirs = include_path
     ext.libraries = ['embree3']
 
 setup(
@@ -18,6 +12,7 @@ setup(
     version='0.2.0',
     ext_modules=ext_modules,
     zip_safe=False,
+    setup_requires=['cython'],
     packages=find_packages(),
     package_data = {'pyembree': ['*.pxd']},
 )
